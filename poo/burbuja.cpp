d2581e1ddc967cdde8a856a4ab101d004ffa0a89
tesagEscolar <113012275+tesagEscolar@users.noreply.github.com>
2023-02-24 15:08:02 -0600

    Add files via upload

---
+++ poo/burbuja.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void leer_arreglo(int *arreglo, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << "Ingrese el elemento " << i << " del arreglo: ";
+    cin >> *(arreglo + i);
+  }
+}
+
+void burbuja(int *arr, int n)
+{
+  for (int i = 0; i < n - 1; i++)
+    for (int j = 0; j < n - i - 1; j++)
+      if (*(arr + j) > *(arr + j + 1))
+        *(arr + j) ^= *(arr + j + 1) ^ (*(arr + j + 1) = *(arr + j));
+}
+
+void imprimir_arreglo(int arr[], int n)
+{
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << ", ";
+  cout << endl;
+}
+
+int main()
+{
+  int arreglo[10];
+  leer_arreglo(arreglo, 10);
+  cout << "Arreglo Original: ";
+  imprimir_arreglo(arreglo, 10);
+  burbuja(arreglo, 10);
+  cout << "Arreglo Ordenado: ";
+  imprimir_arreglo(arreglo, 10);
+  system("pause");
+  return 0;
+}
